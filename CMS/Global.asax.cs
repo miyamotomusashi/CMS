4ffaf067e14caac248d3a0126bb7552367a99a9b
cemelma <cemelma.20@gmail.com>
2014-06-23 15:52:02 +0300

    test

--- CMS/Global.asax.cs
+++ CMS/Global.asax.cs
@@ -23,6 +23,7 @@ namespace CMS
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
         }
     }
 }
